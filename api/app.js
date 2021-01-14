300c82dc98b6f5d08d85129ae72659335bf60550
Elton Santos <eltonaxl@hotmail.com>
2021-01-14 08:36:00 -0300

    Commit all

---
+++ api/app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('./models/Metas');
+const Meta = mongoose.model('Meta');
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+    app.use(cors());
+    next();
+});
+
+mongoose.connect('mongodb://localhost/celke_5', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Conexão com o BD MongoDB realizado com sucesso!");
+}).catch((err) => {
+    console.log("Erro: Conexão com o BD MongoDB não realizado com sucesso: " + err);
+});
+
+app.get('/metas', async (req, res) => {
+    await Meta.find({}).then((metas) => {
+        return res.json({
+            error: false,
+            metas
+        });
+    }).catch((err) => {
+        return res.status(400).json({
+            error: true,
+            message: "Nenhum resgistro encontrado!"
+        });
+    });
+});
+
+app.post('/metas', async (req, res) => {
+
+    await sleep(3000);
+
+    function sleep(ms){
+        return new Promise((resolve) => {
+            setTimeout(resolve, ms);
+        });
+    }
+
+    await Meta.create(req.body, (err) => {
+        if (err) return res.status(400).json({
+            error: true,
+            message: "Erro: Meta não cadastrada com sucesso!"
+        });
+    });
+
+    return res.json({
+        error: false,
+        message: "Meta cadastrada com sucesso!"
+    });
+});
+
+app.listen(8080, () => {
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080");
+});
---
+++ site/pages/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { Jumbotron, Container, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
+
+import Menu from '../components/Menu';
+
+function Home({ data }) {
+    return (
+        <>
+            <Menu />
+
+            <Jumbotron fluid className="list">
+                <style>
+                    {`.list{
+                        background-color: #171941;
+                        padding-top: 30px;
+                        padding-bottom: 150px;
+                        margin-bottom: 0rem !important;
+                    }.title-top{
+                        color: #bf38ac;
+                    }.list-meta{
+                        background-color: #0d345d !important;
+                        border-color: #4a0242 !important;
+                        color: #fff;
+                    }`}
+                </style>
+                <Container>
+                    <h1 className="display-4 text-center title-top">Minha Metas!</h1><hr />
+                    <ListGroup>
+                        {data.metas.map(meta => (
+                            <div key={meta._id}>
+                                <ListGroupItem className="list-meta">
+                                    <ListGroupItemHeading>{meta.name}</ListGroupItemHeading>
+                                    <ListGroupItemText>{meta.description}</ListGroupItemText>
+                                    <ListGroupItemText>{meta.status}</ListGroupItemText>
+                                </ListGroupItem>
+                            </div>
+                        ))}
+                    </ListGroup>
+                </Container>
+            </Jumbotron>
+        </>
+    );
+}
+
+export async function getServerSideProps() {
+    const response = await fetch(`http://localhost:8080/metas`);
+    const data = await response.json();
+    //console.log(data);
+    return { props: { data } };
+}
+
+export default Home;
